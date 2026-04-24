14642f516dc9932e8486514dcca6e8ad39fd0f81
IT23656338 <it23656338@my.sliit.lk>
2026-04-24 20:16:43 +0530

    feat: implement TaskBoard and TaskCard components with supporting task type definitions

--- taskflow-frontend/src/components/tasks/TaskBoard.tsx
+++ taskflow-frontend/src/components/tasks/TaskBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TaskStatus } from '../../types/task.types';
 import type { Task } from '../../types/task.types';
 import TaskColumn from './TaskColumn';
