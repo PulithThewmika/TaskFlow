20221968023e588b814bcfe125283440c0b950c1
IT23656338 <it23656338@my.sliit.lk>
2026-04-25 12:51:38 +0530

    refactor: update TaskControllerTest to use WebMvcTest and remove unnecessary annotations

--- taskflow-backend/src/test/java/com/taskflow/taskflow_backend/controller/TaskControllerTest.java
+++ taskflow-backend/src/test/java/com/taskflow/taskflow_backend/controller/TaskControllerTest.java
@@ -2,27 +2,36 @@ package com.taskflow.taskflow_backend.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import com.taskflow.taskflow_backend.service.TaskService;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
+@WebMvcTest(TaskController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @DisplayName("TaskController Integration Tests")
 class TaskControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private TaskService taskService;
+
     @Test
     @DisplayName("GET /api/projects/1/tasks should be accessible")
     void getTasks_shouldReturnOk() throws Exception {
+        when(taskService.getTasksByProjectId("1")).thenReturn(Collections.emptyList());
+
         mockMvc.perform(get("/api/projects/1/tasks"))
                 .andExpect(status().isOk());
     }
